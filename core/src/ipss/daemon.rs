9f21bfb82c32f636b8fc17d53463c1b8e119c18e
Eduardo Flores <edfloreshz@gmail.com>
2020-07-14 11:33:39 -0700

    Changes to daemon.rs
    [daemon]
    + Signal handling removed for Windows.
    
    
    Former-commit-id: f6fcf6ddf2106a04648a2836a8df27006149236c

--- core/src/ipss/daemon.rs
+++ core/src/ipss/daemon.rs
@@ -1,11 +1,15 @@
 use std::fs::File;
-use notify::{Watcher, RecursiveMode, RawEvent, raw_watcher, op};
 use std::sync::mpsc::channel;
-use crate::replication::engine;
-use dirs;
-use signal_hook::{iterator::Signals, SIGINT};
 use std::{error::Error, thread, process};
+use notify::{Watcher, RecursiveMode, RawEvent, raw_watcher, op};
+use dirs;
 
+use crate::replication::engine;
+
+#[cfg(not(target_os = "windows"))]
+use signal_hook::{iterator::Signals, SIGINT};
+
+#[cfg(not(target_os = "windows"))]
 pub fn init() -> Result<(), Box<dyn Error>> {
   println!("Initializing the daemon...");
   // Create a channel to receive the events.
@@ -39,6 +43,67 @@ pub fn init() -> Result<(), Box<dyn Error>> {
       }
     }
   });
+
+  println!("Waiting for changes...");
+  loop {
+    match rx.recv() {
+      Ok(RawEvent { path: Some(path), op: Ok(op), cookie }) => {
+        println!("{:?} {:?} ({:?})", op, path, cookie);
+        match op {
+          op::CREATE => {
+            match File::open(path) {
+              Ok(file) => engine::add(&file),
+              _ => ()
+            }
+          },
+          op::CLOSE_WRITE => {
+            match File::open(path) {
+              Ok(file) => engine::modify(&file),
+              _ => ()
+            }
+          },
+          op::REMOVE => {
+            match File::open(path) {
+              Ok(file) => engine::remove(&file),
+              _ => ()
+            }
+          },
+          op::RENAME => {
+            match File::open(path) {
+              Ok(file) => engine::rename(&file),
+              _ => ()
+            }
+          },
+          _ => {}
+        }
+      },
+      Ok(event) => println!("broken event: {:?}", event),
+      Err(e) => println!("watch error: {:?}", e),
+    }
+  }
+}
+
+#[cfg(target_os = "windows")]
+pub fn init() -> Result<(), Box<dyn Error>> {
+  println!("Initializing the daemon...");
+  // Create a channel to receive the events.
+  let (tx, rx) = channel();
+
+  // Create a watcher object, delivering raw events.
+  // The notification back-end is selected based on the platform.
+  let mut watcher = raw_watcher(tx).unwrap();
+
+  // Add a path to be watched. All files and directories at that path and
+  // below will be monitored for changes.
+  if let Some(dir) = dirs::download_dir() {
+    watcher.watch(dir, RecursiveMode::Recursive).unwrap();
+  }
+  if let Some(dir) = dirs::document_dir() {
+    watcher.watch(dir, RecursiveMode::Recursive).unwrap();
+  }
+  if let Some(dir) = dirs::desktop_dir() {
+    watcher.watch(dir, RecursiveMode::Recursive).unwrap();
+  }
 
   println!("Waiting for changes...");
   loop {
