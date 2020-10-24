e22a805c602c6787dc22048f76c14e134d727bdf
Eduardo Flores <edfloreshz@gmail.com>
2020-10-24 00:15:18 -0700

    Windows compatability for the daemon.

--- core/src/ipss/daemon.rs
+++ core/src/ipss/daemon.rs
@@ -1,19 +1,19 @@
 #[cfg(not(windows))]
+use signal_hook::{iterator::Signals, SIGINT};
+#[cfg(not(windows))]
 use std::process;
 #[cfg(not(windows))]
 use std::thread;
-#[cfg(not(windows))]
-use std::sync::mpsc::{channel, Receiver, Sender};
 
+use dirs::{desktop_dir, document_dir, download_dir};
 use notify::{op, raw_watcher, RawEvent, RecursiveMode, Watcher};
-use dirs;
-use signal_hook::{iterator::Signals, SIGINT};
-
-use crate::Result;
-use crate::errors::custom::CustomError;
-use crate::replication::engine::*;
 
 use std::path::PathBuf;
+use std::sync::mpsc::{channel, Receiver, Sender};
+
+use crate::errors::custom::CustomError;
+use crate::replication::engine::*;
+use crate::Result;
 
 #[cfg(not(windows))]
 fn manage_signals() -> Result<()> {
@@ -34,17 +34,16 @@ pub fn init() -> Result<()> {
     println!("Initializing the daemon...");
     // Create a channel to receive the events.
     let (tx, rx) = channel();
-    let directories: Vec<Option<PathBuf>> = vec![
-        dirs::download_dir(),
-        dirs::document_dir(),
-        dirs::desktop_dir(),
-    ];
+    let directories: Vec<Option<PathBuf>> = vec![download_dir(), document_dir(), desktop_dir()];
     manage_signals()?;
     watch_events(directories, rx, tx)
 }
 
-fn watch_events(directories: Vec<Option<PathBuf>>, rx: Receiver<RawEvent>, tx: Sender<RawEvent>)
-    -> Result<()> {
+fn watch_events(
+    directories: Vec<Option<PathBuf>>,
+    rx: Receiver<RawEvent>,
+    tx: Sender<RawEvent>,
+) -> Result<()> {
     let mut watcher = raw_watcher(tx).unwrap();
     for directory in directories {
         match directory {
@@ -56,17 +55,23 @@ fn watch_events(directories: Vec<Option<PathBuf>>, rx: Receiver<RawEvent>, tx: S
     loop {
         // TODO: Retrieve the ID from the database in order to keep track of the files.
         match rx.recv() {
-            Ok(RawEvent { path: Some(path), op: Ok(op), cookie}) => {
+            Ok(RawEvent {
+                path: Some(path),
+                op: Ok(op),
+                cookie,
+            }) => {
                 println!("{:?} {:?} ({:?})", op, path, cookie);
                 match op {
                     op::CREATE => add(vec![path.clone()])?,
                     op::WRITE => (),
                     op::RENAME => rename(vec![path.clone()])?,
-                    op::REMOVE => remove(vec![path.clone().into_os_string().into_string().unwrap()])?,
+                    op::REMOVE => {
+                        remove(vec![path.clone().into_os_string().into_string().unwrap()])?
+                    }
                     op::CHMOD => update(vec![path.clone()])?,
                     op::CLOSE_WRITE => update(vec![path.clone()])?,
                     op::RESCAN => (),
-                    _ => println!("Unhandled event")
+                    _ => println!("Unhandled event"),
                 };
             }
             Ok(event) => println!("broken event: {:?}", event),
@@ -80,11 +85,6 @@ pub fn init() -> Result<()> {
     println!("Initializing the daemon...");
     // Create a channel to receive the events.
     let (tx, rx) = channel();
-    let directories: Vec<Option<PathBuf>> = vec![
-        dirs::download_dir(),
-        dirs::document_dir(),
-        dirs::desktop_dir(),
-    ];
-    manage_signals()?;
+    let directories: Vec<Option<PathBuf>> = vec![download_dir(), document_dir(), desktop_dir()];
     watch_events(directories, rx, tx)
 }
